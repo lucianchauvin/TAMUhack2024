9fa952c6395429d92541811bc44510dc09850e35
Lucian Chauvin <lucianchauvin@gmail.com>
2024-01-27 16:20:52 -0600

    finished step function

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 
 type transState struct {
     nextState int
+    write int
     direction bool // right is true
 }
 
@@ -29,10 +30,16 @@ func initialModel() model {
 }
 
 func (m model) step(){ 
-//    curValue := m.head.Value
-//    curValue := m.head.Value
-//    curState := m.state
-//    m.head.Value = 
+    curValue := m.head.Value.(int)
+    curState := m.state
+
+    m.head.Value = m.stateTable[curState][curValue].write
+    m.state = m.stateTable[curState][curValue].nextState
+    if(m.stateTable[curState][curValue].direction){
+        m.head = m.head.Next()
+    }else{
+        m.head = m.head.Prev()
+    }
 }
 
 
