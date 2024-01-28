8530851f3195bb3a8ebdef7997673005655cec92
faefeyfa <faefeyfa@protonmail.com>
2024-01-27 18:05:16 -0600

    add a barely functional feature

--- main.go
+++ main.go
@@ -4,19 +4,20 @@ package main
 import(
 	"fmt"	
 	"os"
-	"strings"
 	list "container/list"
 	tea "github.com/charmbracelet/bubbletea"
 	lipgloss "github.com/charmbracelet/lipgloss"
 )
 
+const HALT = 0
 
 type model struct {
-	tape	*list.List
-	head	*list.Element
-    state   int
-    stateTable [][]transState 
-	run		int //number of steps run
+	tape		*list.List
+	head		*list.Element
+    stateTable	[][]transState 
+    state		int
+	viewWidth	int //number of steps run
+	
 }
 
 func initialModel() model {
@@ -27,10 +28,14 @@ func initialModel() model {
 		head: initTape.Front(),
 		state: 1,
 		stateTable: [][]transState{{{1,1,true}, {1,0,false}},{{1,1,false},{0,0,true}}},
+		viewWidth: 10,
 	}
 }
 
 func (m model) step() model { 
+	if m.state == HALT {
+		return m
+	}
     curValue := m.head.Value.(int)
     curState := m.state
 
@@ -55,6 +60,14 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "n", "s":
 			m = m.step()
+		case "+":
+			if m.viewWidth < 20 {
+				m.viewWidth++
+			}
+		case "-":
+			if m.viewWidth > 2 {
+				m.viewWidth--
+			}
 		case "ctrl+c", "q":
 			return m, tea.Quit
 		}
@@ -66,45 +79,43 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (m model) View() string {
 	s := "The current tape state:\n\n"
 
-	viewWidth := 4;
 	viewHead := m.head;
 
-	for i:=0; i < viewWidth; i++ {
+	for i:=0; i < m.viewWidth; i++ {
 		if viewHead.Prev() == nil {
 			m.tape.InsertBefore(0,viewHead)
 		}
 		viewHead = viewHead.Prev();
 	}
-	for i:=0; i < 2*viewWidth+1; i++ {
+	for i:=0; i < 2*m.viewWidth+1; i++ {
 		s += fmt.Sprintf(" %v", viewHead.Value)
 		if viewHead.Next() == nil {
 			m.tape.InsertAfter(0,viewHead)
 		}
 		viewHead = viewHead.Next();
 	}
-	s += "\n"
+	s += " \n"
 
 		//s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
-	s += strings.Repeat(" ",2*viewWidth+1)
 	s += "^\n"
 	s += fmt.Sprintf("Current state: %v\n", toRune(m.state))
 
-	s += "\nPress r to run, Ctrl-c to stop, s to step, q to quit.\n"
+	s += "\nPress s to step, +/- to see more/less of the tape, q to quit.\n"
 
-	return s
+	return style.Render(s)
 }
 
 var style = lipgloss.NewStyle().
-    Bold(true).
-    Foreground(lipgloss.Color("#FAFAFA")).
-    Background(lipgloss.Color("#7D56F4")).
+    //Foreground(lipgloss.Color("#FAFAFA")).
+    //Background(lipgloss.Color("#7D56F4")).
     PaddingTop(2).
     PaddingLeft(4).
-    Width(22)
+    Width(100).
+	Align(lipgloss.Center)
 
 
 func toRune(i int) string {
-	if i == 0 {
+	if i == HALT {
 		return "HALT"
 	}
     return string(rune('A' + i))
